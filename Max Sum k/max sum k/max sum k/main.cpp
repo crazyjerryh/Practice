d03604df9020c0e4a28ca4cb36a0383b73acd51c
crazyjerryh <maggiexxqhh@163.com>
2015-07-04 20:05:14 +0800

    RMQ and dan diao dui lie

---
+++ Max Sum k/max sum k/max sum k/main.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <memory.h>
+#include <string.h>
+#include <algorithm>
+using namespace std;
+#define maxn 100010
+#define inf 0x3f3f3f3f
+
+int tcase;
+int a[maxn],q[maxn];
+int sum[maxn];
+int n,k;
+int main(void){
+	freopen("debug.txt","r",stdin);
+	scanf("%d",&tcase);
+	while(tcase--){
+		scanf("%d%d",&n,&k);
+		for(int i=1;i<=n;i++){
+			scanf("%d",&a[i]);
+		}
+		for(int i=n+1;i<n+k;i++){
+			a[i]=a[i-n];
+		}
+		
+		sum[1]=0;
+		for(int i=2;i<n+k;i++){
+			sum[i]=sum[i-1]+a[i-1];
+		}
+		
+		int front,last;
+		front=last=0;
+		int ans=-inf,first,second;
+		int l,r;
+		for(int i=1;i<n+k;i++){
+			r=i;
+			while(front<last&&sum[q[last-1]]>sum[i])  last--;
+			q[last++]=i;
+			l=q[front];
+			int t=r-l+1;
+			while(t>k){
+				l=q[++front];
+				t=r-l+1;
+			}
+			if(t<=k){
+				int tmp;
+				if((tmp=sum[r]+a[r]-sum[q[front]])>ans){
+					ans=tmp;
+					first=q[front],second=r;
+				}
+			}
+		}
+		if(first>n)  first=first%n;
+		if(second>n)  second=second%n; 
+		printf("%d %d %d\n",ans,first,second);
+	}
+	return 0;
+} 
